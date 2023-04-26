2e8b416c3b578436927b86dc21410846e1c31ef0
ThaiSon0512 <109654001+ThaiSon0512@users.noreply.github.com>
2023-04-26 18:49:15 +0700

    Add files via upload

---
+++ baitapgiuakiC#/baigiuakiC#2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace InheritanceAndPolymorphismExample
+{
+    class Animal
+    {
+        protected double weight;
+        protected string name;
+
+        public Animal()
+        {
+            weight = 0;
+            name = "";
+        }
+
+        public Animal(double weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public void SetMe(double weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Animal: {0}, Weight: {1} kg", name, weight);
+        }
+    }
+
+    class Lion : Animal
+    {
+        public Lion(double weight, string name) : base(weight, name)
+        {
+            Console.WriteLine("Lion constructor called.");
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Lion: {0}, Weight: {1} kg", name, weight);
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public Tiger(double weight, string name) : base(weight, name)
+        {
+            Console.WriteLine("Tiger constructor called.");
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Tiger: {0}, Weight: {1} kg", name, weight);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal(0, "");
+            Lion lion = new Lion(190, "Simba");
+            Tiger tiger = new Tiger(150, "Shere Khan");
+
+         
+
+            animal.Show();
+            lion.Show();
+            tiger.Show();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baitapgiuakiC#/baitapgiuakiC#/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    class Cylinder
+    {
+        private double radius;
+        private double height;
+        private double baseArea;
+        private double lateralArea;
+        private double totalArea;
+        private double volume;
+
+        public Cylinder(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        public void Process()
+        {
+            baseArea = radius * radius * Math.PI;
+            lateralArea = 2 * Math.PI * radius * height;
+            totalArea = 2 * Math.PI * radius * (height + radius);
+            volume = Math.PI * radius * radius * height;
+        }
+
+        public string Result()
+
+        {
+            string result = "Cylinder Characteristics\n" +
+                            "------------------------\n" +
+                            "Radius: " + radius.ToString() + "\n" +
+                            "Height: " + height.ToString() + "\n" +
+                            "Base Area: " + baseArea.ToString() + "\n" +
+                            "Lateral Area: " + lateralArea.ToString() + "\n" +
+                            "Total Area: " + totalArea.ToString() + "\n" +
+                            "Volume: " + volume.ToString() + "\n";
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, height;
+
+            Console.WriteLine("Enter thu dimenstions of the cylinder");
+            Console.WriteLine("-----------------------------------");
+
+            Console.Write("Enter radius: ");
+            radius = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter height: ");
+            height = double.Parse(Console.ReadLine());
+
+            Cylinder cylinder = new Cylinder(radius, height);
+            cylinder.Process();
+
+            Console.WriteLine(cylinder.Result());
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
